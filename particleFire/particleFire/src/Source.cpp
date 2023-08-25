a18db62d5d3dc46163548b6b75eff166993d34b0
tar1127 <Tanvir.Rasel@gmail.com>
2023-08-24 21:32:16 -0400

    end on envent

--- particleFire/particleFire/src/Source.cpp
+++ particleFire/particleFire/src/Source.cpp
@@ -1,49 +1,53 @@
 #include<stdio.h>
 #include<SDL.h>
 
-
-// screen Dimenstion 
-const int SCREEN_WIDTH = 640; 
-const int SCREEN_HEIGHT = 480; 
-
 int main(int argc, char* argv[]) {
-
+    // screen Dimenstion 
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
     // this is the window 
     SDL_Window* window = NULL; 
 
     // surface contained by the window 
     SDL_Surface* screenSurface = NULL; 
 
-
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-       
-        printf("SDL could not be initilized : %s\n", SDL_GetError()); 
+        printf("SDL could not be initilized : %s\n", SDL_GetError());
     }
     else {
        // create the window here 
-        window = SDL_CreateWindow("SDL Tutorual", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-                                                   SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN); 
+        window = SDL_CreateWindow("SDL Tutorual",
+                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+                                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN); 
 
         if (window == NULL) {
             printf("Window Could not be created: %s", SDL_GetError());
         }
         else {
-            screenSurface = SDL_GetWindowSurface(window); 
 
-            // fill it with a rectange
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x43, 0xab, 0x22)); 
 
-            // update the window 
-            SDL_UpdateWindowSurface(window); 
+            SDL_Event event; 
+            bool gameOver = false; 
 
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+            // game loop 
+            while (!gameOver) {
+                screenSurface = SDL_GetWindowSurface(window);
+                // fill it with a rectange
+                SDL_FillRect(screenSurface,
+                    NULL, 
+                     SDL_MapRGB(screenSurface->format, 0x43, 0x0b, 0x22));
+                // update the window 
+                SDL_UpdateWindowSurface(window);
 
+                while (SDL_PollEvent(&event)) {
+                    if (event.type == SDL_QUIT) {
+                        gameOver = true; 
+                    }
+                }
+            }      
         }
 
     }
-
     SDL_DestroyWindow(window); 
     SDL_Quit();
     return 0;
